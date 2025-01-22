2b865f6ad0dc6590c622e7748bde12cb8622e7b1
al mahjyr <alejo1234536@gmail.com>
2025-01-22 05:37:04 -0400

    se han agregado nuevos cambios en la ventana de userProfile

--- Facturas/accounts/urls.py
+++ Facturas/accounts/urls.py
@@ -6,8 +6,8 @@ from .views import logout_confirm
 
 urlpatterns = [
     path('auth/', views.auth_view, name='auth'),
-    path('register/', views.register, name='register'),
-    path('login/', LoginView.as_view(template_name='accTemplates/login.html'), name='login'),
+    #path('register/', views.register, name='register'),
+   #xpath('login/', LoginView.as_view(template_name='accTemplates/login.html'), name='login'),
     path('logout/', LogoutView.as_view(next_page='auth'), name='logout'),  # Simplificado
     path('profile/', views.user_profile, name='user_profile'),
 ]
