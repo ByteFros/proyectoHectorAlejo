1274c2cded18b04d46dacd017c04de701ab8bb86
Apolo <kesh.andrade@gmail.com>
2025-02-03 11:14:33 +0100

    cambios de pdf actualizados 1

--- Facturas/accounts/static/accounts/js/genInvoicePopup.js
+++ Facturas/accounts/static/accounts/js/genInvoicePopup.js
@@ -1,8 +1,20 @@
-console.log("gen invoice script se ha cargado correctamente ! ! ! !")
 const setupGenInvoicePopup = () => {
     const genInvoiceButton = document.getElementById('genInvoice');
 
-    const showInvoicePopup = () => {
+    const showInvoicePopup = async () => {
+        // Obtener los datos del usuario
+        let companyData = {};
+        try {
+            const response = await fetch('/accounts/profile/data/');
+
+            if (!response.ok) {
+                throw new Error('Error al obtener los datos de la empresa');
+            }
+            companyData = await response.json();
+        } catch (error) {
+            console.error('Error:', error);
+        }
+
         // Crear el HTML del popup
         const popupHtml = `
             <div id="popup" class="popup-overlay">
@@ -95,11 +107,11 @@ const setupGenInvoicePopup = () => {
             // Encabezado con logo y título
             doc.setFontSize(20);
             doc.setTextColor(40);
-            doc.text('Mi Empresa S.L.', 20, 20);
+            doc.text(companyData.username || "Nombre no disponible", 20, 20);
             doc.setFontSize(12);
-            doc.text('Calle Ejemplo, 123', 20, 25);
-            doc.text('Ciudad, País', 20, 30);
-            doc.text('Teléfono: +34 600 000 000', 20, 35);
+            doc.text(companyData.address || "Dirección no disponible", 20, 25);
+            doc.text(companyData.nif || "Teléfono no disponible", 20, 30);
+            doc.text(clientEmail ? `Email: ${companyData.email}` : "Email no disponible", 20, 35);
 
             doc.setFontSize(18);
             doc.text('Factura', 105, 25,{ align: 'left' });
